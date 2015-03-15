678962e732bcd0c34301f92b7128a0ca5b0a454a
rahulait <rahulait@users.noreply.github.com>
2015-03-15 19:03:00 -0400

    Update test.sh

--- test.sh
+++ test.sh
@@ -5,36 +5,36 @@ sum2=0
 sum3=0
 
 echo "Testing process started for Unpatched image."
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "copy tftp://10.193.76.208/unpatched.sh bootflash:" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "run-script bootflash:unpatched.sh" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "delete bootflash:unpatched.sh" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "reload" >> /tmp/pfm_logs 2>&1 &
+sshpass -p "pass1" ssh admin@10.10.10.1 "copy tftp://10.10.10.2/unpatched.sh bootflash:" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "run-script bootflash:unpatched.sh" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "delete bootflash:unpatched.sh" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "reload" >> /tmp/pfm_logs 2>&1 &
 sleep 150
-echo "Testing VSG by sending traffic from traffic VMs."
+echo "Testing by sending traffic from traffic VMs."
 /root/get_test_stats.sh /root/unpatched.txt
-echo "Testing VSG by sending traffic from traffic VMs over."
+echo "Testing by sending traffic from traffic VMs over."
 
 echo "Testing process started for patched (AND/OR) image."
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "copy tftp://10.193.76.208/patched.sh bootflash:" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "run-script bootflash:patched.sh" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "delete bootflash:patched.sh" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "reload" >> /tmp/pfm_logs 2>&1 &
+sshpass -p "pass1" ssh admin@10.10.10.1 "copy tftp://10.10.10.2/patched.sh bootflash:" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "run-script bootflash:patched.sh" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "delete bootflash:patched.sh" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "reload" >> /tmp/pfm_logs 2>&1 &
 sleep 150
-echo "Testing VSG by sending traffic from traffic VMs."
+echo "Testing by sending traffic from traffic VMs."
 /root/get_test_stats.sh /root/patched.txt
-echo "Testing VSG by sending traffic from traffic VMs over."
+echo "Testing by sending traffic from traffic VMs over."
 
 #================================================================
 # For only OR patched VSG
 echo "Testing process started for patched (OR) image."
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "copy tftp://10.193.76.208/or_patched.sh bootflash:" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "run-script bootflash:or_patched.sh" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "delete bootflash:or_patched.sh" >> /tmp/pfm_logs 2>&1
-sshpass -p "Sgate123" ssh admin@10.193.76.214 "reload" >> /tmp/pfm_logs 2>&1 &
+sshpass -p "pass1" ssh admin@10.10.10.1 "copy tftp://10.10.10.2/or_patched.sh bootflash:" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "run-script bootflash:or_patched.sh" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "delete bootflash:or_patched.sh" >> /tmp/pfm_logs 2>&1
+sshpass -p "pass1" ssh admin@10.10.10.1 "reload" >> /tmp/pfm_logs 2>&1 &
 sleep 150
-echo "Testing VSG by sending traffic from traffic VMs."
+echo "Testing by sending traffic from traffic VMs."
 /root/get_test_stats.sh /root/or_patched.txt
-echo "Testing VSG by sending traffic from traffic VMs over."
+echo "Testing by sending traffic from traffic VMs over."
 
 #===============================================================
 
@@ -48,6 +48,6 @@ sum1=$[sum1/j]
 sum2=$[sum2/j]
 sum3=$[sum3/j]
 
-echo "Number of requests per second for patched VSG(AND/OR both) = $sum1"
-echo "Number of requests per second for unpatched VSG = $sum2"
-echo "Number of requests per second for patched VSG(OR Only) = $sum3"
+echo "Number of requests per second for patched image(AND/OR both) = $sum1"
+echo "Number of requests per second for unpatched image = $sum2"
+echo "Number of requests per second for patched image(OR Only) = $sum3"
