562b210e0ce0332c5c4b7f287ed3c96ca773d6a3
rahulait <rahulait@users.noreply.github.com>
2015-03-15 19:03:53 -0400

    Update unpatched.sh

--- unpatched.sh
+++ unpatched.sh
@@ -3,7 +3,7 @@
 configure terminal
 no boot system
 no boot kickstart
-boot system bootflash:nexus-1000v.VSG1.2.gbin
-boot kickstart bootflash:nexus-1000v-kickstart.VSG1.2.gbin
+boot system bootflash:initial_sys_img.gbin
+boot kickstart bootflash:initial_kickstart_img.gbin
 copy r s
 exit
